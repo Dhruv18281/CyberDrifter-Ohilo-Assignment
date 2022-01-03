ac29e77e496906a33a20c86e5fba1acf37a6664d
Dhruv Yadav <dhruv18281@iiitd.ac.in>
2022-01-03 21:26:23 +0530

    Fresh Upload

---
+++ tf-lite-unity-sample-master/Assets/Samples/BlazePose/scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private BlazePoseSample bpsample;
+    private float bend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject bp = GameObject.Find("BlazePose");
+        bpsample = bp.GetComponent<BlazePoseSample>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bend = bpsample.bend;
+
+        transform.Translate(new Vector3(20*bend, 0, 0)*Time.deltaTime);
+    }
+}
---
+++ tf-lite-unity-sample-master/Assets/Samples/BlazePose/scripts/spawnEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+    public float spawnTimemax = 0.75f;
+    public float spawnTimemin = 2.0f;
+    public float curtime = 0;
+    public float offset = 0.05f;
+    private bool running = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void spawn()
+    {
+        GameObject e = Instantiate(enemy) as GameObject;
+        e.transform.position = new Vector3(Random.Range(4f, -4f), 1.37f, 117.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        GameObject bp = GameObject.Find("BlazePose");
+        BlazePoseSample bpsample = bp.GetComponent<BlazePoseSample>();
+        bool d = bpsample.detected;
+        if (!running && d)
+        {
+            StartCoroutine(enemywave());
+            running = true;
+        }
+
+        if(running)
+        {
+            curtime += Time.deltaTime * offset;
+        }
+    }
+
+    IEnumerator enemywave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Mathf.Lerp(spawnTimemax, spawnTimemin, Mathf.Min(1, curtime)));
+            spawn();
+        }
+    }
+}
