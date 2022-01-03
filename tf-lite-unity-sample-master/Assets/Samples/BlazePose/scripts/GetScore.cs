17528aef0ece4bb877ed4688814c1080a1f782fe
Dhruv Yadav <dhruv18281@iiitd.ac.in>
2022-01-03 20:30:47 +0530

    Initial upload

---
+++ tf-lite-unity-sample-master/Assets/Samples/BlazePose/scripts/GetScore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GetScore : MonoBehaviour
+{
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = BlazePoseSample.score.ToString();
+    }
+}
---
+++ tf-lite-unity-sample-master/Assets/Samples/BlazePose/scripts/buildingMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingMove : MonoBehaviour
+{
+    private int speed = 10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime * -1), Space.World);
+        if(this.transform.position.z < 0f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 103f);
+        }
+    }
+}
